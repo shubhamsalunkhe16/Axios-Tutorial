e6f55cb66527636d91f9845e7a2d8df3b35ed5f1
fname git config --global user.name shubham <shubham.salunkhe@gophygital.io>
2022-12-04 21:50:14 +0530

    added axios basics

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Intro from "./components/Intro";
+
+const App = () => {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>Welcome to Axios Tutorial</h1>
+      <Intro />
+    </div>
+  );
+};
+
+export default App;
